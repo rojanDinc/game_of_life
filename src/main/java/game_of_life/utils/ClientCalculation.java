f52c86879e214d0919428247b96c75597925da42
Rojan Dinc <rojan.dinc@valtech.se>
2019-07-30 19:44:42 +0200

    Added helper class for sending cell batches

---
+++ src/main/java/game_of_life/utils/ClientCalculation.java
@@ -0,0 +1,45 @@
+package game_of_life.utils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import game_of_life.models.Cell;
+
+/**
+ * ClientInstruction
+ */
+public class ClientCalculation implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int startCol, endCol;
+    private int startRow, endRow;
+    private ArrayList<ArrayList<Cell>> cellBatch;
+
+    public ClientCalculation(int startCol, int endCol, int startRow, int endRow, ArrayList<ArrayList<Cell>> cellBatch) {
+        this.startCol = startCol;
+        this.endCol = endCol;
+        this.startRow = startRow;
+        this.endRow = endRow;
+        this.cellBatch = cellBatch;
+    }
+
+    public ArrayList<ArrayList<Cell>> getCellBatch() {
+        return cellBatch;
+    }
+
+    public int getStartCol() {
+        return startCol;
+    }
+
+    public int getEndCol() {
+        return endCol;
+    }
+
+    public int getStartRow() {
+        return startRow;
+    }
+
+    public int getEndRow() {
+        return endRow;
+    }
+
+}
