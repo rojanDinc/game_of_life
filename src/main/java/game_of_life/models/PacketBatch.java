3253d2abd948b8e021bba8ae112c802105513203
Rojan Dinc <rojan.dinc@valtech.se>
2019-08-14 22:56:52 +0200

    Renamed file to PacketBatch from ClientCalculation

---
+++ src/main/java/game_of_life/models/PacketBatch.java
@@ -0,0 +1,49 @@
+package game_of_life.models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import game_of_life.models.Cell;
+
+/**
+ * ClientInstruction
+ */
+public class PacketBatch implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int startCol, endCol;
+    private int startRow, endRow;
+    private ArrayList<ArrayList<Cell>> cellBatch;
+
+    public PacketBatch(int startCol, int endCol, int startRow, int endRow, ArrayList<ArrayList<Cell>> cellBatch) {
+        this.startCol = startCol;
+        this.endCol = endCol;
+        this.startRow = startRow;
+        this.endRow = endRow;
+        this.cellBatch = cellBatch;
+    }
+
+    public ArrayList<ArrayList<Cell>> getCellBatch() {
+        return cellBatch;
+    }
+
+    public void setCellBatch(ArrayList<ArrayList<Cell>> cells) {
+        this.cellBatch = cells;
+    }
+
+    public int getStartCol() {
+        return startCol;
+    }
+
+    public int getEndCol() {
+        return endCol;
+    }
+
+    public int getStartRow() {
+        return startRow;
+    }
+
+    public int getEndRow() {
+        return endRow;
+    }
+
+}
