da44e02c5f7f4d3ab87725453749d826f56b5dcf
Rojan Dinc <rojan.dinc@valtech.se>
2019-08-14 22:59:26 +0200

    Added new logic for communicating and aggregation of data

--- src/main/java/game_of_life/network/Server.java
+++ src/main/java/game_of_life/network/Server.java
@@ -1,10 +1,6 @@
 package game_of_life.network;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,17 +8,15 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import game_of_life.models.Cell;
-import game_of_life.utils.ClientCalculation;
+import game_of_life.models.PacketBatch;
 import game_of_life.utils.Constants;
 
 public class Server implements Runnable {
 
     private ServerSocket serverSocket = null;
-    private ArrayList<Socket> clients = new ArrayList<>();
+    private ArrayList<ClientSocket> clients = new ArrayList<>();
     private int clientCount = 0;
     private ExecutorService service;
 
@@ -40,10 +34,10 @@ public class Server implements Runnable {
         // Listen for new clients
         while (true) {
             try {
-                clients.add(serverSocket.accept());
+                clients.add(new ClientSocket(serverSocket.accept()));
                 synchronized (this) {
-                    this.clientCount++;
-                    System.out.println("Client added! Count: " + (this.clientCount + 1));
+                    this.clientCount += 1;
+                    System.out.println("Client added! Count: " + (this.clientCount));
                 }
             } catch (Exception e) {
             }
@@ -56,99 +50,78 @@ public class Server implements Runnable {
 
     public synchronized ArrayList<ArrayList<Cell>> calculate(ArrayList<ArrayList<Cell>> cells) throws Exception {
         // 1. Get all cells
-        final ArrayList<ClientCalculation> batches = new ArrayList<>(getClientCount());
+        int totClients = getClientCount();
+        // final ArrayList<PacketBatch> batches = new ArrayList<>(totClients);
+        final PacketBatch[] batches = new PacketBatch[totClients];
         // 2. Split cells into batches
-        int cols_quotient = Constants.COLS / getClientCount(); // 10
-        int rows_quotient = Constants.ROWS / getClientCount(); // 8
-        for (int i = 0; i < getClientCount(); i++) {
-            if (i < (getClientCount() - 1)) {
-                batches.add(new ClientCalculation(i * cols_quotient, (i + 1) * cols_quotient, i * rows_quotient,
-                        (i + 1) * rows_quotient, cells));
+        int rows_quotient = Constants.ROWS / totClients; // 8
+        for (int i = 0; i < totClients; i++) {
+            if (i < (totClients - 1)) {
+                batches[i] = new PacketBatch(0, Constants.COLS, i * rows_quotient, (i + 1) * rows_quotient, cells);
             } else {
-                batches.add(new ClientCalculation(i * cols_quotient, Constants.COLS, i * rows_quotient, Constants.ROWS,
-                        cells));
+                batches[i] = new PacketBatch(0, Constants.COLS, i * rows_quotient, Constants.ROWS, cells);
             }
         }
         // 3. Distribute batches to clients
-        List<Future<ClientCalculation>> allFutures = new ArrayList<>();
-        for (int i = 0; i < getClientCount(); i++) {
-            allFutures.add(service.submit(new Task(clients.get(i), batches.get(i))));
+        List<Future<PacketBatch>> allFutures = new ArrayList<>();
+        for (int i = 0; i < totClients; i++) {
+            allFutures.add(service.submit(new Task(clients.get(i), batches[i])));
         }
         // 4. Wait for batches to be calculated
-        final ArrayList<ClientCalculation> newBatches = new ArrayList<>(getClientCount());
+        final PacketBatch[] newBatches = new PacketBatch[totClients];
         for (int i = 0; i < allFutures.size(); i++) {
-            Future<ClientCalculation> future = allFutures.get(i);
+            Future<PacketBatch> future = allFutures.get(i);
             try {
-                ClientCalculation result = future.get(); // blocking
-                newBatches.add(result);
+                PacketBatch result = future.get(); // blocking
+                newBatches[i] = result;
             } catch (Exception e) {
                 throw e;
             }
         }
         // 5. Aggregate batches into 2D array
-        final Cell[][] newCells = new Cell[32][24];
-        // newBatches.forEach(batch -> {
-        // for (int col = batch.getStartCol(); col < batch.getEndCol(); col++) {
-        // ArrayList<Cell> _temp = new ArrayList<>();
-        // for (int row = batch.getStartRow(); row < batch.getEndRow(); row++) {
-        // _temp.add(batch.getCellBatch().get(col).get(row));
-        // }
-        // newCells.set(col, _temp);
-        // }
-        // });
-        for (ClientCalculation batch : newBatches) {
-            for (int col = batch.getStartCol(); col < batch.getEndCol(); col++) {
-                for (int row = batch.getStartRow(); row < batch.getEndRow(); row++) {
-                    // _temp.add(batch.getCellBatch().get(col).get(row));
-                    newCells[col][row] = batch.getCellBatch().get(col).get(row);
+        final Cell[][] newCells = new Cell[Constants.COLS][Constants.ROWS];
+        for (int c = 0; c < totClients; c++) {
+            PacketBatch pb = newBatches[c];
+            for (int col = 0; col < Constants.COLS; col++) {
+                for (int row = pb.getStartRow(); row < pb.getEndRow(); row++) {
+                    newCells[col][row] = pb.getCellBatch().get(col).get(row);
                 }
             }
         }
+
         // 6. Return array
-        final List<List<Cell>> finalCells = Arrays.stream(newCells).map(Arrays::asList).collect(Collectors.toList());
-        return to2DArrayList(finalCells);
+        List<ArrayList<Cell>> lists = new ArrayList<ArrayList<Cell>>();
+        for (Cell[] var : newCells) {
+            ArrayList<Cell> temp = new ArrayList<>(Arrays.asList(var));
+            lists.add(temp);
+        }
+        return to2DArrayList(lists);
     }
 
-    private <T> ArrayList<ArrayList<T>> to2DArrayList(List<List<T>> twoDimenArr) {
+    private <T> ArrayList<ArrayList<T>> to2DArrayList(List<ArrayList<T>> twoDimenArr) {
         ArrayList<ArrayList<T>> results = new ArrayList<ArrayList<T>>();
         twoDimenArr.forEach(arr -> results.add(new ArrayList<T>(arr)));
         return results;
     }
 
-    private class Task implements Callable<ClientCalculation> {
-        private Socket client;
-        private ClientCalculation cellBatch;
-        private ObjectOutputStream oos;
-        private ObjectInputStream ois;
+    private class Task implements Callable<PacketBatch> {
+        private PacketBatch cellBatch;
+        private ClientSocket client;
 
         @Override
-        public ClientCalculation call() throws Exception {
+        public PacketBatch call() throws Exception {
             try {
-                oos = new ObjectOutputStream(this.client.getOutputStream());
-                ois = new ObjectInputStream(this.client.getInputStream());
-                oos.writeObject(cellBatch);
+                client.getOos().writeObject(cellBatch);
                 // Wait for calculation of batch to return from client
-                ClientCalculation newCellBatch = (ClientCalculation) ois.readObject();
+                PacketBatch newCellBatch = (PacketBatch) client.getOis().readObject();
                 return newCellBatch;
             } catch (Exception e) {
                 e.printStackTrace();
                 throw e;
-            } finally {
-                try {
-                    // oos.reset();
-                    // ois.reset();
-                    oos.close();
-                    ois.close();
-                    oos = null;
-                    ois = null;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    throw e;
-                }
             }
         }
 
-        public Task(Socket client, ClientCalculation cellBatch) {
+        public Task(ClientSocket client, PacketBatch cellBatch) {
             this.client = client;
             this.cellBatch = cellBatch;
         }
