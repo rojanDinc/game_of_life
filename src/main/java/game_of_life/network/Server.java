346c83c2ea5013ec60c65b005488c8621f71119d
Rojan Dinc <rojan.dinc@valtech.se>
2019-08-18 22:56:28 +0200

    Rename of class name

--- src/main/java/game_of_life/network/Server.java
+++ src/main/java/game_of_life/network/Server.java
@@ -8,9 +8,10 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.IntStream;
 
 import game_of_life.models.Cell;
-import game_of_life.models.PacketBatch;
+import game_of_life.models.CellBatch;
 import game_of_life.utils.Constants;
 
 public class Server implements Runnable {
@@ -50,29 +51,29 @@ public class Server implements Runnable {
 
     public synchronized ArrayList<ArrayList<Cell>> calculate(ArrayList<ArrayList<Cell>> cells) throws Exception {
         // 1. Get all cells
-        int totClients = getClientCount();
+        final int totClients = getClientCount();
         // final ArrayList<PacketBatch> batches = new ArrayList<>(totClients);
-        final PacketBatch[] batches = new PacketBatch[totClients];
+        final CellBatch[] batches = new CellBatch[totClients];
         // 2. Split cells into batches
-        int rows_quotient = Constants.ROWS / totClients; // 8
+        final int rows_quotient = Constants.ROWS / totClients; // 8
         for (int i = 0; i < totClients; i++) {
             if (i < (totClients - 1)) {
-                batches[i] = new PacketBatch(0, Constants.COLS, i * rows_quotient, (i + 1) * rows_quotient, cells);
+                batches[i] = new CellBatch(0, Constants.COLS, i * rows_quotient, (i + 1) * rows_quotient, cells);
             } else {
-                batches[i] = new PacketBatch(0, Constants.COLS, i * rows_quotient, Constants.ROWS, cells);
+                batches[i] = new CellBatch(0, Constants.COLS, i * rows_quotient, Constants.ROWS, cells);
             }
         }
         // 3. Distribute batches to clients
-        List<Future<PacketBatch>> allFutures = new ArrayList<>();
+        List<Future<CellBatch>> allFutures = new ArrayList<>();
         for (int i = 0; i < totClients; i++) {
             allFutures.add(service.submit(new Task(clients.get(i), batches[i])));
         }
         // 4. Wait for batches to be calculated
-        final PacketBatch[] newBatches = new PacketBatch[totClients];
+        final CellBatch[] newBatches = new CellBatch[totClients];
         for (int i = 0; i < allFutures.size(); i++) {
-            Future<PacketBatch> future = allFutures.get(i);
+            Future<CellBatch> future = allFutures.get(i);
             try {
-                PacketBatch result = future.get(); // blocking
+                CellBatch result = future.get(); // blocking
                 newBatches[i] = result;
             } catch (Exception e) {
                 throw e;
@@ -81,7 +82,7 @@ public class Server implements Runnable {
         // 5. Aggregate batches into 2D array
         final Cell[][] newCells = new Cell[Constants.COLS][Constants.ROWS];
         for (int c = 0; c < totClients; c++) {
-            PacketBatch pb = newBatches[c];
+            CellBatch pb = newBatches[c];
             for (int col = 0; col < Constants.COLS; col++) {
                 for (int row = pb.getStartRow(); row < pb.getEndRow(); row++) {
                     newCells[col][row] = pb.getCellBatch().get(col).get(row);
@@ -92,8 +93,7 @@ public class Server implements Runnable {
         // 6. Return array
         List<ArrayList<Cell>> lists = new ArrayList<ArrayList<Cell>>();
         for (Cell[] var : newCells) {
-            ArrayList<Cell> temp = new ArrayList<>(Arrays.asList(var));
-            lists.add(temp);
+            lists.add(new ArrayList<Cell>(Arrays.asList(var)));
         }
         return to2DArrayList(lists);
     }
@@ -104,16 +104,16 @@ public class Server implements Runnable {
         return results;
     }
 
-    private class Task implements Callable<PacketBatch> {
-        private PacketBatch cellBatch;
+    private class Task implements Callable<CellBatch> {
+        private CellBatch cellBatch;
         private ClientSocket client;
 
         @Override
-        public PacketBatch call() throws Exception {
+        public CellBatch call() throws Exception {
             try {
                 client.getOos().writeObject(cellBatch);
                 // Wait for calculation of batch to return from client
-                PacketBatch newCellBatch = (PacketBatch) client.getOis().readObject();
+                CellBatch newCellBatch = (CellBatch) client.getOis().readObject();
                 return newCellBatch;
             } catch (Exception e) {
                 e.printStackTrace();
@@ -121,7 +121,7 @@ public class Server implements Runnable {
             }
         }
 
-        public Task(ClientSocket client, PacketBatch cellBatch) {
+        public Task(ClientSocket client, CellBatch cellBatch) {
             this.client = client;
             this.cellBatch = cellBatch;
         }
