fce247d810c67f0f115ca58661e3dc7f53480923
Rojan Dinc <rojan.dinc@valtech.se>
2019-08-19 20:15:26 +0200

    Cleanup

--- src/main/java/game_of_life/network/Server.java
+++ src/main/java/game_of_life/network/Server.java
@@ -8,7 +8,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.stream.IntStream;
 
 import game_of_life.models.Cell;
 import game_of_life.models.CellBatch;
